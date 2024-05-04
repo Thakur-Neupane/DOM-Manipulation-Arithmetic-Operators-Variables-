eddb13968d3acfff91471d8c5049da9d34c3136e
Thakur Neupane <thakur.neupane.neupane@gmail.com>
2024-05-04 14:36:18 +1000

    Arrays and Objects

--- Basics/script.js
+++ Basics/script.js
@@ -56,16 +56,66 @@
 // const age = 50;
 // document.getElementById("output").textContent = age;
 
-let x = 13;
-let y = 3;
-var add = x + y;
-let subt = x - y;
-const mult = x * y;
-let div = x / y;
-let modulos = x % y;
+// let x = 13;
+// let y = 3;
+// var add = x + y;
+// let subt = x - y;
+// const mult = x * y;
+// let div = x / y;
+// let modulos = x % y;
 
-document.getElementById("add").textContent = `Sum is ` + add;
-document.getElementById("subt").textContent = `Subtraction is ` + subt;
-document.getElementById("mult").textContent = `Multiplication is` + mult;
-document.getElementById("div").textContent = `Division is` + div;
-document.getElementById("modulos").textContent = `Modulos is` + modulos;
+// document.getElementById("add").textContent = `Sum is :` + add;
+// document.getElementById("subt").textContent = `Subtraction is :` + subt;
+// document.getElementById("mult").textContent = `Multiplication is :` + mult;
+// document.getElementById("div").textContent = `Division is :` + div;
+// document.getElementById("modulos").textContent = `Modulos is :` + modulos;
+
+// interview questions
+// 1) What is variable?
+// 2)What is a syntax in programming language?
+// 3)Comments in JS.
+// 4)what are the different ways of declaring variables in JS?
+// 5)When to use var let or const?
+// 6)How to name a variable?
+// 7)Garbage Collection.
+
+// Data Types
+// primitive                                                     Reference
+// String , number, boolean , undefined, null                     -Object, Array, Function
+
+// // STRING
+// const name= "Dented Code"
+// const city ='Sydney'
+// const gender =`Male`
+// const language = String(1);
+
+// document.getElementById("output").textContent=type of name
+
+// // NUMBER
+// const number= 100
+// var n1= 5.5
+// const num = -55
+// const num2 = 123456787889
+
+// // Boolean
+// const bool1= false
+// const bool2= true
+// const bool3= 1===1;
+
+// // undefined Data Type
+// var thakur;
+// const stupid = undefined;
+
+// // Null type
+// let stupid2 = null;
+
+// // Symbol
+// let id = Symbol('id')
+
+// Big Int
+
+// Array Object and Functions
+// Array
+
+const Arg = ["milk", "egg", "banana", "papaya"];
+const randomarray = [1, 2, 2.4, false, [hello], 1000, "string"];
